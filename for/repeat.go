4e80b8c68ceb5c83756feda9d3d5ee0fc5b72bb5
a-mellor <alex.mellor@honeycomb.tv>
2019-07-19 17:50:48 +0100

    Repeattttt

---
+++ for/repeat.go
@@ -0,0 +1,11 @@
+package iteration
+
+// Repeat returns character 5 times
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
