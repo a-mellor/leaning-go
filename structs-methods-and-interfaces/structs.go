69535b071c53d60867c844258557b467a67ea764
a-mellor <alex.mellor@honeycomb.tv>
2019-08-03 07:49:46 +0100

    Add comments

--- structs-methods-and-interfaces/structs.go
+++ structs-methods-and-interfaces/structs.go
@@ -2,10 +2,12 @@ package structs
 
 import "math"
 
+// Shape is implemented by anything that can tell us its Area
 type Shape interface {
 	Area() float64
 }
 
+// Rectangle has the dimensions of a rectangle
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -16,6 +18,7 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+// Circle represents a circle
 type Circle struct {
 	Radius float64
 }
@@ -25,7 +28,7 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-// Return the perimiter of a given width and height
+// Returns the perimiter of a rectangle
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
